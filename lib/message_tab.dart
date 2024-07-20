98910f50943edf06623262daa99b11b6cd69d04b
HARSH <basedharsh@gmail.com>
2024-07-20 09:31:54 +0530

    Lets- Go: basic Ui done

--- lib/message_tab.dart
+++ lib/message_tab.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:godrage/providers/session_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
 
 class MessagesTab extends StatefulWidget {
   final ScrollController scrollController;
@@ -21,6 +20,15 @@ class MessagesTab extends StatefulWidget {
 
 class _MessagesTabState extends State<MessagesTab> {
   bool isTyping = false;
+  int? highlightedIndex;
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = widget.scrollController;
+    _simulateIncomingMessage();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,8 +65,16 @@ class _MessagesTabState extends State<MessagesTab> {
           return ChatMessage.fromFirestore(messageData);
         }).toList();
 
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          _scrollController.animateTo(
+            _scrollController.position.maxScrollExtent,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeOut,
+          );
+        });
+
         return ListView.builder(
-          controller: widget.scrollController,
+          controller: _scrollController,
           itemCount: messages.length + (isTyping ? 1 : 0),
           itemBuilder: (context, index) {
             if (index == messages.length && isTyping) {
@@ -72,9 +88,7 @@ class _MessagesTabState extends State<MessagesTab> {
                 alignment: message.isUserMessage
                     ? Alignment.centerRight
                     : Alignment.centerLeft,
-                child: message.isUserMessage
-                    ? _buildUserMessage(message)
-                    : _buildBotMessage(message),
+                child: _buildMessage(message, index),
               ),
             );
           },
@@ -83,69 +97,30 @@ class _MessagesTabState extends State<MessagesTab> {
     );
   }
 
-  Widget _buildUserMessage(ChatMessage message) {
-    return IntrinsicWidth(
-      child: Container(
-        padding: const EdgeInsets.all(12.0),
-        margin: const EdgeInsets.symmetric(vertical: 4.0),
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.75,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.blue[100],
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(15.0),
-            bottomLeft: Radius.circular(15.0),
-            bottomRight: Radius.circular(15.0),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: const Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              message.message,
-              style: const TextStyle(
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              DateFormat('hh:mm a')
-                  .format(DateTime.now()), // Replace with actual timestamp
-              style: TextStyle(
-                color: Colors.grey[600],
-                fontSize: 10,
-              ),
-            ),
-          ],
-        ),
+  Widget _buildMessage(ChatMessage message, int index) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: MediaQuery.of(context).size.width * 0.75,
       ),
-    );
-  }
-
-  Widget _buildBotMessage(ChatMessage message) {
-    return IntrinsicWidth(
-      child: Container(
+      child: AnimatedContainer(
+        duration: const Duration(seconds: 2),
         padding: const EdgeInsets.all(12.0),
         margin: const EdgeInsets.symmetric(vertical: 4.0),
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width * 0.75,
-        ),
         decoration: BoxDecoration(
-          color: Colors.green[50],
-          borderRadius: const BorderRadius.only(
-            topRight: Radius.circular(15.0),
-            bottomLeft: Radius.circular(15.0),
-            bottomRight: Radius.circular(15.0),
-          ),
+          color: highlightedIndex == index
+              ? (message.isUserMessage ? Colors.blue[200] : Colors.green[100])
+              : (message.isUserMessage ? Colors.blue[100] : Colors.green[50]),
+          borderRadius: message.isUserMessage
+              ? const BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                )
+              : const BorderRadius.only(
+                  topRight: Radius.circular(15.0),
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0),
+                ),
           boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.5),
@@ -155,50 +130,47 @@ class _MessagesTabState extends State<MessagesTab> {
             ),
           ],
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Icon(
-                  Icons.smart_toy_outlined,
+        child: message.isUserMessage
+            ? Text(
+                message.message,
+                style: const TextStyle(
                   color: Colors.black,
                 ),
-                const SizedBox(width: 8.0),
-                Expanded(
-                  child: Text(
-                    message.message,
-                    style: const TextStyle(
-                      color: Colors.black87,
-                      fontSize: 16.0,
+              )
+            : Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.green[300],
+                    child: const Icon(
+                      Icons.smart_toy_outlined,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.copy, color: Colors.black87, size: 16),
-                  onPressed: () {
-                    Clipboard.setData(ClipboardData(text: message.message));
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Message copied to clipboard'),
+                  const SizedBox(width: 8.0),
+                  Expanded(
+                    child: Text(
+                      message.message,
+                      style: const TextStyle(
+                        color: Colors.black87,
+                        fontSize: 16.0,
                       ),
-                    );
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 5),
-            Text(
-              DateFormat('hh:mm a')
-                  .format(DateTime.now()), // Replace with actual timestamp
-              style: TextStyle(
-                color: Colors.grey[600],
-                fontSize: 10,
+                    ),
+                  ),
+                  IconButton(
+                    icon:
+                        const Icon(Icons.copy, color: Colors.black87, size: 16),
+                    onPressed: () {
+                      Clipboard.setData(ClipboardData(text: message.message));
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Message copied to clipboard'),
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
@@ -213,7 +185,7 @@ class _MessagesTabState extends State<MessagesTab> {
             padding: const EdgeInsets.all(12.0),
             margin: const EdgeInsets.symmetric(vertical: 4.0),
             constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.75,
+              maxWidth: MediaQuery.of(context).size.width * 0.50,
             ),
             decoration: BoxDecoration(
               color: Colors.green[50],
@@ -234,18 +206,42 @@ class _MessagesTabState extends State<MessagesTab> {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Icon(
-                  Icons.smart_toy_outlined,
-                  color: Colors.black,
+                CircleAvatar(
+                  backgroundColor: Colors.green[300],
+                  child: const Icon(
+                    Icons.smart_toy_outlined,
+                    color: Colors.white,
+                  ),
                 ),
                 const SizedBox(width: 8.0),
-                const CircularProgressIndicator(),
+                const Expanded(
+                  child: TypingIndicator(),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _simulateIncomingMessage() {
+    setState(() {
+      isTyping = true;
+    });
+
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        isTyping = false;
+        highlightedIndex = 3; // Simulating an incoming message index
+      });
+
+      Future.delayed(const Duration(seconds: 2), () {
+        setState(() {
+          highlightedIndex = null;
+        });
+      });
+    });
   }
 }
 
@@ -259,6 +255,61 @@ class ChatMessage {
     return ChatMessage(
       message: data['message'] ?? '',
       isUserMessage: data['isUser'] ?? false,
+    );
+  }
+}
+
+class TypingIndicator extends StatelessWidget {
+  const TypingIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: const [
+        Dot(),
+        SizedBox(width: 4),
+        Dot(),
+        SizedBox(width: 4),
+        Dot(),
+      ],
+    );
+  }
+}
+
+class Dot extends StatefulWidget {
+  const Dot({Key? key}) : super(key: key);
+
+  @override
+  _DotState createState() => _DotState();
+}
+
+class _DotState extends State<Dot> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _controller,
+      child: const CircleAvatar(
+        radius: 4,
+        backgroundColor: Colors.grey,
+      ),
     );
   }
 }
